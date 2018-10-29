eca2512d6675e82454a25b738724d49e45f50a00
hilarious23 <s.positex11@gmail.com>
2018-10-29 10:01:17 +0900

    in the process

---
+++ nuxt/nuxt-blog-practice/app/components/TheHeader.vue
@@ -0,0 +1,10 @@
+<template lang="pug">
+  el-menu(mode="horizontal" :router="true")
+    el-menu-item(index="1" style="pointer-events:none;") Nuxt Diary App
+    el-menu-item(index="2" :route="{ path: '/posts' }") 投稿一覧
+
+    no-ssr
+      el-menu-item(index="4" style="float: right;" :route="{ path: '/' }")
+        span Login
+    el-menu-item(index="5" style="float: right;" :route="{ path: '/posts/new' }") 新規投稿
+</template>
